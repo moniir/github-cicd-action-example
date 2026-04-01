022148936379b5aca112f3225c387df295c8996f
Monir Hossain <monir198@gmail.com>
2026-04-01 12:35:20 +0600

    initiate changes

--- src/main/java/com/example/githubcicdactions/GithubCicdActionsApplication.java
+++ src/main/java/com/example/githubcicdactions/GithubCicdActionsApplication.java
@@ -2,12 +2,18 @@ package com.example.githubcicdactions;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class GithubCicdActionsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GithubCicdActionsApplication.class, args);
-	}
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome to ci/cd hands-on project!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubCicdActionsApplication.class, args);
+    }
 
 }
